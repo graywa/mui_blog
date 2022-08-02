cbc28b124c97a7842342419ade751216310856c7
graywa <74241140+graywa@users.noreply.github.com>
2022-08-02 12:40:49 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Settings } from '@mui/icons-material'
+import { Button } from '@mui/material'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Button variant='contained' disabled>
+        Hello World
+      </Button>
+      <Button
+        variant='outlined'
+        color='success'
+        size='large'
+        startIcon={<Settings />}
+      >
+        Hello World
+      </Button>
+      <Button variant='text' size='small'>
+        Hello Worldd
+      </Button>
+    </>
+  )
 }
 
-export default App;
+export default App
