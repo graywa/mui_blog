d801fedbbcbf072a438ce9652ea0cbf1683f57cf
graywa <74241140+graywa@users.noreply.github.com>
2022-08-03 23:46:57 +0300

    add modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,23 +1,104 @@
-import { Fab, Tooltip } from '@mui/material'
-import { Add as AddIcon } from '@mui/icons-material'
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material'
+import {
+  Add as AddIcon,
+  EmojiEmotions,
+  Image,
+  VideoCameraBack,
+  PersonAdd,
+  DateRange,
+} from '@mui/icons-material'
+import { useState } from 'react'
+import ava1 from './assets/ava1.jpg'
 
 const Add = () => {
+  const [isModalOpen, setModalOpen] = useState(false)
+
   return (
-    <Tooltip
-      title='Add'
-      sx={{
-        position: 'fixed',
-        bottom: 30,
-        left: {
-          xs: 'calc(50% - 28px)',
-          md: 30,
-        },
-      }}
-    >
-      <Fab size='large' color='primary' aria-label='add'>
-        <AddIcon />
-      </Fab>
-    </Tooltip>
+    <>
+      <Tooltip
+        title='Add'
+        sx={{
+          position: 'fixed',
+          bottom: 30,
+          left: {
+            xs: 'calc(50% - 28px)',
+            md: 30,
+          },
+        }}
+        onClick={() => setModalOpen(true)}
+      >
+        <Fab size='large' color='primary' aria-label='add'>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+      <Modal
+        open={isModalOpen}
+        onClose={() => setModalOpen(false)}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          width={360}
+          height={280}
+          bgcolor='white'
+          padding={4}
+          borderRadius={5}
+        >
+          <Typography variant='h6' color='gray' textAlign='center'>
+            Create Post
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 2,
+              margin: '20px 0',
+            }}
+          >
+            <Avatar src={ava1} width={30} height={30} />
+            <Typography fontWeight={500}>Dimon</Typography>
+          </Box>
+          <TextField
+            id='standard-multiline-static'
+            multiline
+            rows={3}
+            placeholder='How are you?'
+            variant='standard'
+            sx={{ width: '100%' }}
+          />
+          <Stack direction='row' gap={2} mt={2} mb={2}>
+            <EmojiEmotions color='primary' />
+            <Image color='secondary' />
+            <VideoCameraBack color='success' />
+            <PersonAdd color='error' />
+          </Stack>
+          <ButtonGroup
+            variant='contained'
+            aria-label='outlined primary button group'
+            fullWidth
+          >
+            <Button>Post</Button>
+            <Button sx={{width: '100px'}}><DateRange/></Button>
+          </ButtonGroup>
+        </Box>
+      </Modal>
+    </>
   )
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,5 @@
 import {
-  Avatar,  
+  Avatar,
   Card,
   CardActions,
   CardContent,
@@ -9,51 +9,51 @@ import {
   IconButton,
   Typography,
 } from '@mui/material'
-import { MoreVert, Favorite, FavoriteBorder, Share } from '@mui/icons-material'
+import {MoreVert, Favorite, FavoriteBorder, Share} from '@mui/icons-material'
 import poella from './assets/paella.jpg'
 
 const Post = () => {
   return (
     <Card sx={{mb: 2}}>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: 'red' }} aria-label='recipe'>
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label='settings'>
-              <MoreVert />
-            </IconButton>
-          }
-          title='Shrimp and Chorizo Paella'
-          subheader='September 14, 2016'
-        />
-        <CardMedia
-          component='img'
-          height='20%'
-          image={poella}
-          alt='Paella dish'
-        />
-        <CardContent>
-          <Typography variant='body2' color='text.secondary'>
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label='add to favorites'>
-            <Checkbox
-              icon={<FavoriteBorder />}
-              checkedIcon={<Favorite sx={{ color: 'red' }} />}              
-            />
+      <CardHeader
+        avatar={
+          <Avatar sx={{bgcolor: 'red'}} aria-label='recipe'>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label='settings'>
+            <MoreVert/>
           </IconButton>
-          <IconButton aria-label='share'>
-            <Share />
-          </IconButton>
-        </CardActions>
-      </Card>
+        }
+        title='Shrimp and Chorizo Paella'
+        subheader='September 14, 2016'
+      />
+      <CardMedia
+        component='img'
+        height='20%'
+        image={poella}
+        alt='Paella dish'
+      />
+      <CardContent>
+        <Typography variant='body2' color='text.secondary'>
+          This impressive paella is a perfect party dish and a fun meal to
+          cook together with your guests. Add 1 cup of frozen peas along with
+          the mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label='add to favorites'>
+          <Checkbox
+            icon={<FavoriteBorder/>}
+            checkedIcon={<Favorite sx={{color: 'red'}}/>}
+          />
+        </IconButton>
+        <IconButton aria-label='share'>
+          <Share/>
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 };
 
